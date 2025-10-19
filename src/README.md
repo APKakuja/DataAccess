b946f70d6007b0763da0e52296e80773c6fca402
APKakuja <adrianpetrisorkakuja@gmail.com>
2025-10-19 22:17:52 +0200

    Añadido README con capturas de todos los endpoints

---
+++ src/README.md
@@ -0,0 +1,22 @@
+# DataAccess
+
+END Point 1: Init DB
+Inicia la base de datos
+![Captura Init DB](imagenes/ENDPOINTInitDB.png)
+
+END POINT 2: Find all customers para seleccionar a todos los customers existentes en la DB
+![Captura FindAll DB](imagenes/ENDPOINTFindAll.png)
+
+
+END POINT 3: Find by ID, para seleccionar solo el customer que queremos en concreto.
+![Captura FindID DB](imagenes/ENDPOINTFindByID.png)
+
+
+END POINT 4: Create Client, crea un nuevo cliente dentro de la base de datos(el ID se crea de forma automatica)
+![Captura Create Client DB](imagenes/ENDPOINTCreateClient.png)
+
+END POINT 5: Update Client, actualiza la información del cliente que queremos dentro de la DB
+![Captura Update DB](imagenes/ENDPOINTUpdateClient.png)
+
+END POINT 6: Eliminate Client, elimina el cliente que especifiquemos de dentro de la DB
+![Captura Eliminate DB](imagenes/ENDPOINTEliminateClient.png)
--- src/main/java/com/ra2/jdbctemplate_h2/controller/CustomerController.java
+++ src/main/java/com/ra2/jdbctemplate_h2/controller/CustomerController.java
@@ -9,13 +9,14 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/jdbctemplate") // <-- raíz de tu app según tu ejemplo
+@RequestMapping("/jdbctemplate")
 public class CustomerController {
 
     @Autowired
     private CustomerRepository repository;
 
     // Inicializar BD y datos de ejemplo
+    // URL ENDPOINT: http://localhost:8080/jdbctemplate/init-db
     @PostMapping("/init-db")
     public ResponseEntity<String> initDb() {
         repository.createTable();
@@ -24,18 +25,21 @@ public class CustomerController {
     }
 
     // Listar todos los clientes
+    // END POINT URL: http://localhost:8080/jdbctemplate/findAllCustomers
     @GetMapping("/findAllCustomers")
     public List<Customer> findAllCustomers() {
         return repository.findAll();
     }
 
     // Buscar cliente por id
+    // END POINT URL: http://localhost:8080/jdbctemplate/customer/1
     @GetMapping("/customer/{id}")
     public Customer findById(@PathVariable Long id) {
         return repository.findById(id);
     }
 
     // Crear cliente
+    // END POINT URL: http://localhost:8080/jdbctemplate/customer
     @PostMapping("/customer")
     public ResponseEntity<String> createCustomer(@RequestBody Customer customer) {
         repository.createCustomer(customer);
@@ -43,6 +47,7 @@ public class CustomerController {
     }
 
     // Actualizar cliente
+    // URL END POINT: http://localhost:8080/jdbctemplate/customer
     @PutMapping("/customer")
     public ResponseEntity<String> updateCustomer(@RequestBody Customer customer) {
         repository.updateCustomer(customer);
@@ -50,6 +55,7 @@ public class CustomerController {
     }
 
     // Eliminar cliente
+    // URL END POINT: http://localhost:8080/jdbctemplate/customer/"id of desired customer here"
     @DeleteMapping("/customer/{id}")
     public ResponseEntity<String> deleteCustomer(@PathVariable Long id) {
         repository.deleteCustomer(id);
